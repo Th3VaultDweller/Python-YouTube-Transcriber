8caa01f7b79f6f2906a3276af714d4d88ddf9efe
Th3VaultDweller <aztec242@yandex.ru>
2024-03-31 23:50:02 +0300

    Added mp3 downloading

--- youtube_transcriber.py
+++ youtube_transcriber.py
@@ -1,17 +1,15 @@
+import os
+import time
+
+import whisper
 from pytube import YouTube
 
 # определяем url видео и скачиваем
-video_url = input("Вставьте url-адрес видео, которое хотите скачать: ")
+video_url = input("Вставьте url-адрес видео: ")
 video_info = YouTube(video_url)
-print(f"\n[INFO] Скачиваю видео <<{video_info.title}>>\n")
+print(f"\n[INFO] Скачиваю <<{video_info.title}>>\n")
 
-video_download = (
-    YouTube(video_url)
-    .streams.get_highest_resolution()
-    .download(
-        output_path="D:\YandexDisk\Education\IT\Python\Learning Python\Python-YouTube-Transcriber\downloaded_videos"
-    )
-)
+video = YouTube(video_url).streams.filter(only_audio=True).first()
 print(
     f"""[INFO]
       Автор: {video_info.author}
@@ -22,3 +20,16 @@ print(
       Количество просмотров: {video_info.views}
       Продолжительность видео в секундах: {video_info.length}"""
 )
+
+# указываем папку для сохранённых аудиофайлов и скачиваем файл
+destination = "D:\YandexDisk\Education\IT\Python\Learning Python\Python-YouTube-Transcriber\downloaded_audio"
+
+out_file = video.download(output_path=destination)
+
+# сохраняем
+base, ext = os.path.splitext(out_file)
+new_file = base + ".mp3"
+os.rename(out_file, new_file)
+
+# выводим результат
+print(video_info.title + " успешно скачан.")
