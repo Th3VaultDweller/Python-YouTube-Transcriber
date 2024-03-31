59a73c60c8191ae61823745fba3d37aa8d6b0d72
Th3VaultDweller <aztec242@yandex.ru>
2024-04-01 01:24:16 +0300

    Added loop for video urls

--- youtube_transcriber.py
+++ youtube_transcriber.py
@@ -4,32 +4,39 @@ import time
 import whisper
 from pytube import YouTube
 
+import downloaded_audio
+
 # определяем url видео и скачиваем
-video_url = input("Вставьте url-адрес видео: ")
-video_info = YouTube(video_url)
-print(f"\n[INFO] Скачиваю <<{video_info.title}>>\n")
+video_urls = {
+    "https://www.youtube.com/shorts/wt4Ct-aW6tQ?feature=share",
+    "https://www.youtube.com/shorts/SMB1TsTJYjY?feature=share",
+    "https://www.youtube.com/shorts/Fogu5ovZoDM?feature=share",
+}
 
-video = YouTube(video_url).streams.filter(only_audio=True).first()
-print(
-    f"""[INFO]
-      Автор: {video_info.author}
-      Дата загруки: {video_info.publish_date}
-      Описание: {video_info.description}
-      Обложка: {video_info.thumbnail_url}
-      Рейтинг видео: {video_info.rating}
-      Количество просмотров: {video_info.views}
-      Продолжительность видео в секундах: {video_info.length}"""
-)
+for i, video_url in enumerate(video_urls):
+    video_info = YouTube(video_url)
+    print(i)
+    print(f"\n[INFO] Скачиваю <<{video_info.title}>>\n")
 
-# указываем папку для сохранённых аудиофайлов и скачиваем файл
-destination = "D:\YandexDisk\Education\IT\Python\Learning Python\Python-YouTube-Transcriber\downloaded_audio"
+    video = YouTube(video_url).streams.filter(only_audio=True).first()
+    print(
+        f"""[INFO]
+        Автор: {video_info.author}
+        Дата загруки: {video_info.publish_date}
+        Обложка: {video_info.thumbnail_url}
+        Количество просмотров: {video_info.views}
+        Продолжительность видео в секундах: {video_info.length}"""
+    )
 
-out_file = video.download(output_path=destination)
+    # указываем папку для сохранённых аудиофайлов и скачиваем файл
+    destination = "D:\YandexDisk\Education\IT\Python\Learning Python\Python-YouTube-Transcriber\downloaded_audio"
 
-# сохраняем
-base, ext = os.path.splitext(out_file)
-new_file = base + ".mp3"
-os.rename(out_file, new_file)
+    out_file = video.download(output_path=destination)
 
-# выводим результат
-print(video_info.title + " успешно скачан.")
+    # сохраняем
+    base, ext = os.path.splitext(out_file)
+    new_file = base + ".mp3"
+    os.rename(out_file, new_file)
+
+    # выводим результат
+    print(f"\n[INFO] <<{video_info.title}>> успешно скачан.\n")
