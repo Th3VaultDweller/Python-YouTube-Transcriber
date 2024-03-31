68beb107966ba97b791412093f8b286b90405c38
Th3VaultDweller <aztec242@yandex.ru>
2024-03-31 16:33:47 +0300

    Testing video download

--- youtube_transcriber.py
+++ youtube_transcriber.py
@@ -1,1 +1,24 @@
 from pytube import YouTube
+
+# определяем url видео и скачиваем
+video_url = input("Вставьте url-адрес видео, которое хотите скачать: ")
+video_info = YouTube(video_url)
+print(f"\n[INFO] Скачиваю видео <<{video_info.title}>>\n")
+
+video_download = (
+    YouTube(video_url)
+    .streams.get_highest_resolution()
+    .download(
+        output_path="D:\YandexDisk\Education\IT\Python\Learning Python\Python-YouTube-Transcriber\downloaded_videos"
+    )
+)
+print(
+    f"""[INFO]
+      Автор: {video_info.author}
+      Дата загруки: {video_info.publish_date}
+      Описание: {video_info.description}
+      Обложка: {video_info.thumbnail_url}
+      Рейтинг видео: {video_info.rating}
+      Количество просмотров: {video_info.views}
+      Продолжительность видео в секундах: {video_info.length}"""
+)
