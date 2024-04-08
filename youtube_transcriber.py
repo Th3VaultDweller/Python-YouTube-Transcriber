0c14ce35b30ba85e3a9ce50b40a12298b64baa66
Th3VaultDweller <aztec242@yandex.ru>
2024-04-08 20:08:33 +0300

    Added functions and user input

--- youtube_transcriber.py
+++ youtube_transcriber.py
@@ -7,69 +7,94 @@ from tqdm import tqdm
 
 start_app_time = timer()  # отсчёт с начала работы программы
 
-# определяем url видео и скачиваем
-video_urls = {
-    "https://youtu.be/3Xu0j56dhBE",
-}
 
-for i, video_url in enumerate(video_urls):
-    video_info = YouTube(video_url)
-    print(i)
-    print(f"\n[INFO] Скачиваю <<{video_info.title}>>\n")
+def download_audio():
+    """Скачивает аудиофайл с YouTube по заданному url-адресу"""
 
-    # берём из видеофайла только аудиодорожку
-    video = YouTube(video_url).streams.filter(only_audio=True).first()
-    print(
-        f"""[INFO]
-        Автор: {video_info.author}
-        Дата загруки: {video_info.publish_date}
-        Обложка: {video_info.thumbnail_url}
-        Количество просмотров: {video_info.views}
-        Продолжительность видео в секундах: {video_info.length}"""
-    )  # дополнительная информация о видеоролике
+    # определяем url видео
+    video_urls = []
+    url = input("Вставьте ссылку на видео: ")
+    video_urls.append(url)
 
     # указываем папку для сохранённых аудиофайлов и скачиваем файл
-    destination = "D:\YandexDisk\Education\IT\Python\Learning Python\Python-YouTube-Transcriber\downloaded_audio"
+    destination = input("Укажите полный путь сохранения аудиофайла: ")
 
-    out_file = video.download(output_path=destination)
+    # проходимся по каждой ссылке из списка
+    for i, video_url in enumerate(video_urls):
+        video_info = YouTube(video_url)
+        print(i)
+        print(f"\n[INFO] Скачиваю <<{video_info.title}>>\n")
 
-    # сохраняем
-    base, ext = os.path.splitext(out_file)
-    new_file = base + ".mp3"
-    os.rename(out_file, new_file)
+        # берём из видеофайла только аудиодорожку
+        video = YouTube(video_url).streams.filter(only_audio=True).first()
+        print(
+            f"""[INFO]
+            Автор: {video_info.author}
+            Дата загруки: {video_info.publish_date}
+            Обложка: {video_info.thumbnail_url}
+            Количество просмотров: {video_info.views}
+            Продолжительность видео в секундах: {video_info.length}"""
+        )  # дополнительная информация о видеоролике
 
-    # выводим результат
-    print(f"\n[INFO] <<{video_info.title}>> успешно скачан.\n")
+        out_file = video.download(output_path=destination)
+
+        # сохраняем
+        base, ext = os.path.splitext(out_file)
+        new_file = base + ".mp3"
+        os.rename(out_file, new_file)
+
+        # выводим результат
+        print(f"\n[INFO] <<{video_info.title}>> успешно скачан.\n")
 
 
-# определяем папку со скачанными файлами
-root_folder = "D:\YandexDisk\Education\IT\Python\Learning Python\Python-YouTube-Transcriber\downloaded_audio"
+# def make_meta_table():
+#     """Создаёт метаразметку в соответствии с вводом информации от пользователя"""
 
-model = whisper.load_model("large")  # выбираем между medium и large
 
-# определяем количество файлов в папке и в подпапках
-num_files = sum(
-    1
-    for dirpath, dirnames, filenames in os.walk(root_folder)
-    for filename in filenames
-    if filename.endswith(".mp3")
-)
+def transcribe_audio():
+    """Транскрибирует аудиофайл"""
+    # выбираем модель Whisper
+    model = whisper.load_model(
+        input("Укажите название модели (tiny, base, small, medium или large): ")
+    )
 
-# Транскрибируем файлы и выводим прогресс-бар
-print(f"[INFO] Начинаю создание текста...\n")
-with tqdm(total=num_files, desc="\n[INFO] Готовность текста") as pbar:
-    for dirpath, dirnames, filenames in os.walk(root_folder):
-        for filename in filenames:
-            if filename.endswith(".mp3"):
-                filepath = os.path.join(dirpath, filename)
-                result = model.transcribe(filepath, fp16=False, verbose=True)
-                transcription = result["text"]
+    # определяем папку со скачанными файлами
 
-                # Записываем информацию в текстовый файл
-                filename_no_ext = os.path.splitext(filename)[0]
-                with open(os.path.join(dirpath, filename_no_ext + ".txt"), "w") as f:
-                    f.write(transcription)
-                pbar.update(1)
+    root_folder = input("Укажите полный путь к скачанным файлам: ")
+
+    # определяем количество файлов в папке и в подпапках
+    num_files = sum(
+        1
+        for dirpath, dirnames, filenames in os.walk(root_folder)
+        for filename in filenames
+        if filename.endswith(".mp3")
+    )
+
+    # Транскрибируем файлы и выводим прогресс-бар
+    print(f"[INFO] Начинаю создание текста...\n")
+    with tqdm(total=num_files, desc="\n[INFO] Готовность текста") as pbar:
+        for dirpath, dirnames, filenames in os.walk(root_folder):
+            for filename in filenames:
+                if filename.endswith(".mp3"):
+                    filepath = os.path.join(dirpath, filename)
+                    result = model.transcribe(filepath, fp16=False, verbose=True)
+                    transcription = result["text"]
+
+                    # Записываем информацию в текстовый файл
+                    filename_no_ext = os.path.splitext(filename)[0]
+                    with open(
+                        os.path.join(dirpath, filename_no_ext + ".txt"), "w"
+                    ) as f:
+                        f.write(transcription)
+                    pbar.update(1)
+
+
+download_audio()
+transcribe_audio()
+
+# def make_new_line():
+#     """Переносит каждое предложение в файле после точки на новую строку"""
+
 
 overall_app_time = timer() - start_app_time  # общий подсчёт времени
 
